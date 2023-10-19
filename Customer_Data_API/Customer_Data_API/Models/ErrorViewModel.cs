68b7a673b084f91a5da37c19223ae5d51019cc81
Praveen Chamod <praveenchamod23@gmail.com>
2023-10-19 15:05:51 +0530

    Remove the project folder

--- Customer_Data_API/Customer_Data_API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Customer_Data_API.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
